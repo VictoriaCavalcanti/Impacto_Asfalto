a4e646cb574a9648db7b1d2ba2b2634981d20764
FastShowDev <zlFastShow@hotmail.com>
2023-10-13 05:54:41 -0300

    Adiciona map do dataset

---
+++ src/utils/maps.py
@@ -0,0 +1,45 @@
+dataset_map = {
+  'mix' : 0,
+  'plants': 1,
+
+  'type_binder': 2,
+  'binder' : 3,
+  'performances': 4, #2
+  'rap' : 5,
+
+  'type_binder_additive': 6,
+  'binder_additive': 7,
+  'type_mix_additive': 8,
+  'mix_additive': 9,
+
+  'gradations' : 10,
+  'type_mix': 11,
+  'temperatures': 12,
+
+  'aggregate_lime' : 13,
+  'aggregate_portland' : 14,
+  'aggregate_ras' : 15,
+  'aggregate_crusher' : 16,
+
+  'CO2material': 17, 
+  'CO2transport': 18,
+  'CO2production' : 19,
+  'CO2total' : 20,
+
+  'NRPRfuel_mat' : 21,
+  'NRPRfuel_tra' : 22,
+  'NRPRfuel_pro' : 23,
+  'NRPRfuel_total' : 24,
+
+  'NRPRmat_mat' : 25,
+  'NRPRmat_tra' : 26,
+  'NRPRmat_pro' : 27,
+  'NRPRmat_total' : 28,
+
+  'SM_mat' : 29,
+  'SM_tra' : 30,
+  'SM_pro' : 31,
+  'SM_total' : 32,
+
+  'urls' : 33,
+}
