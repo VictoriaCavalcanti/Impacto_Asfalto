3f0f32109161160f29164a6025577c0bd4ab71a3
FastShowDev <zlFastShow@hotmail.com>
2023-10-13 09:08:43 -0300

    Adiciona funções para printar na tela

--- src/utils/printer.py
+++ src/utils/printer.py
@@ -1,2 +1,17 @@
-def print_test():
+def print_dataset_error(abbreviation: bool, state_url: str, result: tuple[int, list, list, int]):
+  print(f'Dataset inválido - Estado {abbreviation} - Link {state_url}')
+  print('\nFalharam as seguintes colunas:')
+  for i in range(len(result[1])):
+    print(f'Coluna: {result[1][i]} - Tamanho: {result[2][i]} - Esperado: {result[3]}')
+  print('\nEscrita cancelada\n')
+
+
+def print_sheet_status(expected: bool, result: tuple[int, list, list, int]):
+  if (result[0] != expected):
+    print(f'Planilha inválida - Ao todo escreveu-se {result[0]} linhas')
+    print(f"O esperado era {expected} linhas")
+  print(f'Local de salvamento: {result[1]}\n')
+
+
+def print_summary():
   pass
