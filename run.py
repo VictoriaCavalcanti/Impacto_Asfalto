e8265e1e066734a03a7b4609555ff8891cd7490d
FastShowDev <zlFastShow@hotmail.com>
2023-10-13 05:57:59 -0300

    Adiciona arquivo para iniciar o programa

---
+++ run.py
@@ -0,0 +1,8 @@
+import sys
+sys.path.insert(1, 'C:\\Users\\zlFas\\Documents\\Impacto_Asfalto\\src')
+
+from src.app import Scrappy
+
+if (__name__ == '__main__'):
+  scrappy = Scrappy()
+  scrappy.run()
