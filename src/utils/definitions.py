5f0e026f943471e34e2f900bf17c03b5be7ffe39
FastShowDev <zlFastShow@hotmail.com>
2023-10-13 05:57:03 -0300

    Adiciona protótipo de definições

---
+++ src/utils/definitions.py
@@ -0,0 +1,12 @@
+## Arquivo com as constantes de classes, id's etc
+class PageOneDefinitions:
+  def __init__(self) -> None:
+    pass
+
+  @property
+  def gradationID(self):
+    return 'th_1'
+  
+  @property
+  def performanceID(self):
+    return 'tk_1'
