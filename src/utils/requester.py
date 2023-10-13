b6437faa3be7b9dc71877e41ee9572e047ed7096
FastShowDev <zlFastShow@hotmail.com>
2023-10-13 09:09:08 -0300

    Adiciona funções auxliares para carregar todos os links

--- src/utils/requester.py
+++ src/utils/requester.py
@@ -8,3 +8,15 @@ def get_html(url):
       return soup
   else:
     raise ValueError('BAD URL', url)
+
+def get_all_states_urls():
+  urls = []
+  prefix = 'https://asphaltepd.org'
+  page = get_html('https://asphaltepd.org/published/')
+  data_table = page.find('table', id='data-table')
+  links = data_table.find_all('a')
+
+  for link in links:
+      urls.append(prefix + link.get('href'))
+      
+  return urls
