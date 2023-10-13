a2ffbc9031280a8b3a9f838d9cdc125bc95a03aa
FastShowDev <zlFastShow@hotmail.com>
2023-10-13 05:50:10 -0300

    Adiciona modularização dos links que o Scrappy irá rodar

---
+++ src/links/__init__.py
@@ -0,0 +1,2 @@
+from links.states_urls import states_urls
+from links.test_urls import test_urls
---
+++ src/links/states_urls.py
@@ -0,0 +1,5 @@
+states_urls = [
+  'https://asphaltepd.org/published/CA/',
+  'https://asphaltepd.org/published/CO/',
+  'https://asphaltepd.org/published/AL/',
+]
