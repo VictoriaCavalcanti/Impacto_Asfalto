b59877251fb246716f363b51c01aba55a4502ef3
Gabriel Di Vanna Camargo <109106987+FastShowDev@users.noreply.github.com>
2023-10-14 17:08:11 -0300

    Update run.py

--- run.py
+++ run.py
@@ -3,10 +3,11 @@ import os
 
 # Obtenha o caminho absoluto do diretório em que este script está localizado.
 diretorio_atual = os.path.abspath(os.path.dirname(__file__))
+src_path = os.path.join(diretorio_atual, "src")
 
 # Adicione o diretório atual ao sys.path se ainda não estiver lá.
-if diretorio_atual not in sys.path:
-    sys.path.append(diretorio_atual)
+if src_path not in sys.path:
+    sys.path.append(src_path)
 
 from src.app import Scrappy
 
