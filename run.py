dee9b5d60a464f01b71b5c52b787d5fcb51ceb89
Gabriel Di Vanna Camargo <109106987+FastShowDev@users.noreply.github.com>
2023-10-14 16:42:32 -0300

    Update run.py

--- run.py
+++ run.py
@@ -4,5 +4,5 @@ sys.path.insert(1, 'C:\\Users\\zlFas\\Documents\\Impacto_Asfalto\\src')
 from src.app import Scrappy
 
 if (__name__ == '__main__'):
-  scrappy = Scrappy(None, False, False)
+  scrappy = Scrappy(None, False, True)
   scrappy.run()
